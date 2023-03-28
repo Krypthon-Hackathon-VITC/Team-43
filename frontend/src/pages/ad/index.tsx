0de2327f628b07907ff71471381222184e56a1bb
RandomOgre101 <94991501+RandomOgre101@users.noreply.github.com>
2023-03-28 05:41:16 +0530

    FE - initial ad page

---
+++ frontend/src/pages/ad/index.tsx
@@ -0,0 +1,12 @@
+import { PageLayout } from "@layouts/PageLayout";
+import React from "react";
+
+const ViewAds = () => {
+  return (
+    <PageLayout title="Ad Manager" isAdPage>
+      <p>Hello</p>
+    </PageLayout>
+  );
+};
+
+export default ViewAds;
